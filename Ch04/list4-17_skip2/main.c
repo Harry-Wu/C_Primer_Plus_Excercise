9647946530f9a6b9fde5aaeb8bce100f8b376bf0
bit_harrywu <wuxiaohuaer@gmail.com>
2019-05-03 22:19:52 +0800

    list4-7: 添加

---
+++ Ch04/list4-17_skip2/main.c
@@ -0,0 +1,12 @@
+/* skip2.c ----跳过输入的头两个整数 */
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+	int n;
+	printf("Please enter 3 integers:\n");
+	scanf("%*d %*d %d", &n);  //将输入的第3个值赋于n
+	printf("The last integer was %d\n", n);
+
+	return 0;
+}
+
